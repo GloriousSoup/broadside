1362b0d9a6e239c215edcd9dd3f3b611343e95db
Richard Fabian <rfabian@22cans.com>
2015-02-23 22:35:53 +0000

    making opengl stuff compile for windows

--- CApp.h
+++ CApp.h
@@ -4,9 +4,16 @@
 #ifndef _CAPP_H_
 #define _CAPP_H_
 
+#if _WIN32
+#define WINGDIAPI
+#define APIENTRY
+#include <SDL.h>
+#include <GL/gl.h>
+#else
 #include <SDL2/SDL.h>
 #include <GL/gl.h>
 #include <GL/glu.h>
+#endif
 
 //==============================================================================
 class CApp {
